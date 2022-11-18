42fa2cef879d02f7e52a4033fcbf9cb6d0a106f8
uygareren <73404092+uygareren@users.noreply.github.com>
2022-11-18 15:48:37 +0300

    Add files via upload
    Week7 Lab

---
+++ app/src/main/java/com/example/week7/BoardListener.java
@@ -0,0 +1,16 @@
+package com.example.week7;
+
+public interface BoardListener {
+    byte NO_ONE = 0;
+    byte PLAYER_1 = 1;
+    byte PLAYER_2 = 2;
+
+    void playedAt(byte player, byte row, byte col);
+
+    void gameEnded(byte winner);
+
+    void invalidplay(byte row, byte col);
+
+}
+
+
